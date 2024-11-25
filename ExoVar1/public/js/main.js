af91888373743fb4ba7e7a2cfa676240c2298f62
Tiiickk <fabian.cange@hotmail.fr>
2024-11-25 09:37:43 +0100

    exercice done

---
+++ ExoVar1/public/js/main.js
@@ -0,0 +1,21 @@
+// Exercice 1
+
+let maVariableLet = "Bonjour"
+const maVariableConst = 42
+
+// Exercice 2
+
+let nbr = 5
+nbr = 10
+console.log(nbr)
+
+// Exercice 3
+
+let prenom = "Jean"
+let age = 30
+let estEtudiant = true
+
+// Exercice 4
+
+const pi = 3.14159
+// pi = 3.14
