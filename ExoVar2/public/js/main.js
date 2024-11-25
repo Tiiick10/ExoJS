a59047a6ba6fbde89c6325b073152903f4ac9e9b
Tiiickk <fabian.cange@hotmail.fr>
2024-11-25 10:00:24 +0100

    exo 2 et 3

---
+++ ExoVar2/public/js/main.js
@@ -0,0 +1,70 @@
+// Partie 1
+
+// 1ere var string
+
+let ma_phrase = "J'aime les tartines et j'en mange"
+
+console.log(ma_phrase)
+
+// 2eme var string
+
+let nombre_par_jours = "4 par jours"
+
+// Addition des 2 var
+
+let resultat_concatenation = ma_phrase + " " + nombre_par_jours
+console.log(resultat_concatenation)
+
+// 3eme var mais int
+
+let troiseme = 5
+
+// 4eme var mais int
+
+let quatrieme = 35
+
+// Addition de 3 et 4
+
+let resultat_addition = troiseme + quatrieme
+console.log(resultat_addition)
+
+// Addition de 1 et 3
+
+let resultat_final = ma_phrase + " " + troiseme
+console.log(resultat_final)
+
+// Partie 2
+
+// 1ere var string
+
+let ma_premiere_phrase = "Salut tout le monde"
+
+console.log(ma_premiere_phrase)
+
+// 2eme var string
+
+let ma_deuxieme_phrase = "Aurevoir !"
+
+console.log(ma_deuxieme_phrase)
+
+// Réaffectation de la 1ere var avec la valeur de la 2eme
+
+ma_premiere_phrase = ma_deuxieme_phrase
+
+console.log(ma_premiere_phrase)
+
+// Partie 3
+
+// Déclarez deux variables
+
+let number1 = 4
+let number2 = "6"
+
+// Différents opérateurs entre deux variables
+
+console.log(number1 > number2)
+console.log(number1 < number2)
+console.log(number1 == number2)
+console.log(number1 != number2)
+console.log(number1 === number2)
+console.log(number1 !== number2) 
