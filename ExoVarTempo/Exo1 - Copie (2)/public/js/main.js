5783266275d0354cc995693711f05818e1a52610
Tiiickk <fabian.cange@hotmail.fr>
2024-11-25 10:02:24 +0100

    structure var temporaire

---
+++ ExoVarTempo/Exo1 - Copie (2)/public/js/main.js
@@ -0,0 +1,39 @@
+// Exercice 1
+
+let monString = "Ceci est une chaîne de caractères"
+console.log(monString)
+
+let monInt = 42
+alert(monInt)
+
+// Exercice 2
+
+let reponseUtilisateur = prompt("Veuillez entrer quelque chose :")
+
+alert(reponseUtilisateur)
+
+// Exercice 3
+
+let prenom = prompt("Quel est ton prénom ?")
+
+alert("Bonjour " + prenom)
+
+// Exercice 3.1
+
+let age = prompt("Quel est ton âge ?")
+
+alert("Tu as " + age + " ans")
+
+// Exercice 4
+
+let nom = prompt("Quel est ton nom de famille ?")
+let prenomComplet = prenom + " " + nom
+
+console.log(prenomComplet)
+
+// Exercice 5
+
+let nomMaj = nom.toUpperCase()
+let variableComplete = nomMaj + " " + prenom
+
+alert("Bonjour " + variableComplete)
